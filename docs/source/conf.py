aa3c850841c3da0f96b27b2f4461d6ec774e3779
Damien Bezborodov <dbezborodov@gmail.com>
2024-02-20 03:36:47 +1030

    Autodoc?

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,4 +1,10 @@
-project = 'ISO 6346 Freight Containers: Coding, Identification and Marking'
+project = 'ISO 6346'
 copyright = '2024 Damien Bezborodov'
 author = 'Damien Bezborodov'
 master_doc = 'index'
+
+html_title = 'ISO 6346 Freight Containers: Coding, Identification and Marking'
+
+extensions = [
+    'sphinx.ext.autodoc',
+]
