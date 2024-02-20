21a8671aa8c20078823d7b71c98789c537898cf7
Damien Bezborodov <dbezborodov@gmail.com>
2024-02-21 05:07:06 +1030

    Test other methods.

--- tests.py
+++ tests.py
@@ -10,5 +10,26 @@ class TestIso6346(unittest.TestCase):
                          'Incorrect checkdigit.')
 
 
+    def test_validate(self):
+        self.assertEqual(iso6346.validate('ZEPU0037255'), True,
+                         'Incorrect validation, should pass.')
+
+
+    def test_format(self):
+        self.assertEqual(iso6346.format('ZEPU0037255'), 'ZEPU 003725 5',
+                         'Incorrect format.')
+
+
+    def test_format_box(self):
+        self.assertEqual(iso6346.format('ZEPU0037255', box=True), 'ZEPU 003725 [5]',
+                         'Incorrect format.')
+
+
+
+    def test_normalise(self):
+        self.assertEqual(iso6346.normalize(' ZEPU-003725 [5]'), 'ZEPU0037255',
+                         'Incorrect normalisation.')
+
+
 if __name__ == '__main__':
     unittest.main()
